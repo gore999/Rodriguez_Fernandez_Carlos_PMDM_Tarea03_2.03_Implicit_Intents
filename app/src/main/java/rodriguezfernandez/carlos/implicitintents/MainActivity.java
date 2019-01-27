0cd7a7c2bb3604c442d6c9f0a90d1f80a855809e
Carlos <carloscoucho@gmail.com>
2019-01-27 11:26:59 +0100

    2.3 Task 1 bis: Correccion de errores: Se cambian los textView por los EditText.

--- app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
@@ -1,13 +1,21 @@
 package rodriguezfernandez.carlos.implicitintents;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEditText=findViewById(R.id.website_edittext);
+    }
+
+    public void openWebsite(View view) {
     }
 }
