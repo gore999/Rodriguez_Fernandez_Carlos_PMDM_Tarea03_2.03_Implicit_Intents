781bbe9ba6fe683411a420ea7f94220b04867cf6
Carlos <carloscoucho@gmail.com>
2019-01-27 11:39:43 +0100

    2.3 Task 2: implementar el metodo openWebsite. Encontrar una Activity que pueda resolver lo que le pedimos.

--- app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
@@ -1,8 +1,11 @@
 package rodriguezfernandez.carlos.implicitintents;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -17,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openWebsite(View view) {
+        //Obtener el texto del EditText destinado a urls:
+        String url=mWebsiteEditText.getText().toString();
+        Uri webpage=Uri.parse(url);// convierte el texto en url.
+        Intent intent=new Intent(Intent.ACTION_VIEW,webpage);
+        /*Buscar una activity que pueda manejar el intent implicito con
+        *    resolveActivity y
+        *    getPackageManager
+        */
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
     }
 }
