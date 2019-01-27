4a71080dbd1539d330049dfcada6904e892e1c50
Carlos <carloscoucho@gmail.com>
2019-01-27 12:01:38 +0100

    2.3 Task 4: implementar el metodo shareText. La aplicacion queda completa.

--- app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
+++ app/src/main/java/rodriguezfernandez/carlos/implicitintents/MainActivity.java
@@ -3,6 +3,7 @@ package rodriguezfernandez.carlos.implicitintents;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,14 +13,14 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
+    private EditText mShareTextEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEditText=findViewById(R.id.website_edittext);
         mLocationEditText=findViewById(R.id.location_edittext);
-
-
+        mShareTextEditText=findViewById(R.id.share_edittext);
     }
 
     public void openWebsite(View view) {
@@ -48,5 +49,11 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Log.d("ImplicitIntents","Can´t handle this!");
         }
+    }
+
+    public void shareText(View view) {
+        String txt=mShareTextEditText.getText().toString();
+        String mimeType="text/plain";
+        ShareCompat.IntentBuilder.from(this).setType(mimeType).setChooserTitle("Share this ").setText(txt).startChooser();
     }
 }
